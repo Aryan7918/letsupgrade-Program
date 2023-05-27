58bf85c68eb155b95981f565719d9e7aa0eee3d2
Patel Aryankumar <92469184+Aryan7918@users.noreply.github.com>
2023-05-27 21:31:08 +0530

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,6 @@
+// Create a function that takes two numbers as parameters and returns their sum.
+function twonumber(num1,num2){
+    return num1+num2;
+}
+var result=twonumber(5,15);
+console.log(result);
